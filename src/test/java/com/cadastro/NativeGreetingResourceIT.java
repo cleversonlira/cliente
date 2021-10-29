a8fbcaeb96f9d23881440ebd5a582a1e3fdb9c1c
quarkusio <no-reply@quarkus.io>
2021-10-29 12:39:05 +0000

    Initial commit

---
+++ src/test/java/com/cadastro/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.cadastro;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
