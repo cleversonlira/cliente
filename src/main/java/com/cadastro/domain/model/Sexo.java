00f1ffd3e96dd8c33e07c8fa5b1e1a427be5a8b2
cleversonlira <cleverson_lira@outlook.com>
2021-11-14 11:31:41 -0300

    alterando a porta

--- src/main/java/com/cadastro/domain/model/Sexo.java
+++ src/main/java/com/cadastro/domain/model/Sexo.java
@@ -1,29 +1,8 @@
 package com.cadastro.domain.model;
 
 public enum Sexo {
-    FEMININO{
-        @Override
-        public int obterId() {
-            return 1;
-        }
-    },
-    MASCULINO{
-        @Override
-        public int obterId() {
-            return 2;
-        }
-    },
-    NAO_BINARIO{
-        @Override
-        public int obterId() {
-            return 3;
-        }
-    },
-    PREFIRO_NAO_DECLARAR{
-        @Override
-        public int obterId() {
-            return 4;
-        }
-    };
-    public abstract int obterId();
+    FEMININO,
+    MASCULINO,
+    NAO_BINARIO,
+    PREFIRO_NAO_DECLARAR;
 }
